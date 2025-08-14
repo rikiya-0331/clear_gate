a342f1f2684efee43b1b88dbdd9ec45b7644773d
Rikiya Nakao <baseball.r.n@icloud.com>
2025-08-14 10:52:04 +0900

    feat: クイズのシードデータを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ApplicationRecord
   belongs_to :category
   has_many :answer_choices, dependent: :destroy
   has_many :quiz_results, dependent: :destroy
+
+  accepts_nested_attributes_for :answer_choices
 end
