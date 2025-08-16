d062dd76ae5b8a1d405d7463ea957fc37e28d3fa
Rikiya Nakao <baseball.r.n@icloud.com>
2025-08-16 10:12:31 +0900

    feat: 質問一覧ページの基本構造を作成

---
+++ app/javascript/controllers/qa_card_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="qa-card"
+export default class extends Controller {
+  static targets = ["japanese", "button"]
+
+  toggle() {
+    this.japaneseTarget.classList.toggle("hidden")
+
+    const isHidden = this.japaneseTarget.classList.contains("hidden")
+    this.buttonTarget.textContent = isHidden ? "日本語訳を見る" : "日本語訳を隠す"
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  resources :questions, only: [:index]
 end
