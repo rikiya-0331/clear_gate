0a635b30f849749f21402267c1bb07f63f2bdc29
Rikiya Nakao <baseball.r.n@icloud.com>
2025-08-18 22:07:19 +0900

    feat: 質問詳細ページの基本機能とルーティングを実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,9 @@ class QuestionsController < ApplicationController
     @categories = Category.all
     @questions = Question.all.includes(:category)
   end
+
+  def show
+    @question = Question.find(params[:id])
+    @correct_answer_choice = @question.answer_choices.find_by(is_correct: true)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  resources :questions, only: [:index]
+  resources :questions, only: [:index, :show]
 end
