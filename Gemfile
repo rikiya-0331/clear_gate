b699ed640450d21156231c59d29dfb069c0d2436
Rikiya Nakao <baseball.r.n@icloud.com>
2025-10-22 15:11:46 +0900

    feat: GoogleアカウントによるSNSログイン機能の実装

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ gem "rails-i18n"
 
 gem "dotenv-rails", groups: [:development, :test]
 gem "google-cloud-text_to_speech"
+gem "omniauth-google-oauth2"
+gem "omniauth-rails_csrf_protection"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   # Replaced :validatable module with manual validations
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :email, uniqueness: { case_sensitive: false, message: "は正しくありません" }
-  validates :password, presence: true, length: { in: 6..128 }, confirmation: true
+  # validates :password, presence: true, length: { in: 6..128 }, confirmation: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+    end
+  end
 end
