048feb4f9ee9150ab1a92e6c9140ac32b3d18466
Rikiya Nakao <baseball.r.n@icloud.com>
2025-10-27 13:09:26 +0900

    fix: RSpec/AnyInstance RuboCop違反を修正

--- spec/requests/quizzes_spec.rb
+++ spec/requests/quizzes_spec.rb
@@ -109,7 +109,7 @@ RSpec.describe 'Quizzes', type: :request do
       it 'returns a correct response and URL for the next question' do
         # Mock the questions that will be used to populate session[:quiz_question_ids]
         # Ensure category.questions.order(:id).all returns exactly [question1, question2]
-        allow_any_instance_of(Category).to receive_message_chain(:questions, :order, :all).and_return([question1, question2])
+        allow(category).to receive_message_chain(:questions, :order, :all).and_return([question1, question2])
 
         post start_quizzes_path, params: { category_id: category.id }
 
